ac824a0fd0e4c2a29dc2708d0e74b02365bcdb8b
Huzaifa-Islam <official.huzaifa.coder@gmail.com>
2024-10-26 10:00:28 +0530

    Exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,22 +2,23 @@ const express = require("express");
 
 const app = express();
 
-
-
-
-
-
-app.use("/profile",(req,res)=>{
-    res.send("Hello from Profile")
+//This will only handle GET call to /user
+app.get("/user", (req,res)=>{
+    res.send({firstName:"Huzaifa", lastName:"Islam"})
+})
+app.post("/user", async(req,res)=>{
+    console.log(req.body);
+    //savind data to database
+    res.send("Data saved successfully to db!!")
 })
 
+app.delete("/user", (req,res)=>{
+    res.send("Deleted successfully")
+})
 
+//This will match all the HTTP method API calls to /test
 app.use("/test",(req,res)=>{
     res.send("Hello from Test")
-})
-
-app.use("/",(req,res)=>{
-    res.send("Hello from Home")
 })
 
 app.listen(3000,()=>{
